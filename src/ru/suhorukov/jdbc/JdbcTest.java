d2d78953d740aeeaa7fca63c8d2b681ef9b73e7a
ViTr <ViTr@list.ru>
2016-05-05 20:05:15 +0400

    feat: Изучение JDBC

--- src/ru/suhorukov/jdbc/JdbcTest.java
+++ src/ru/suhorukov/jdbc/JdbcTest.java
@@ -1,11 +1,18 @@
 package ru.suhorukov.jdbc;
 
+import java.sql.CallableStatement;
 import java.sql.Connection;
+import java.sql.DatabaseMetaData;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
+import java.sql.Savepoint;
 import java.sql.Statement;
+import java.sql.Types;
+
+import com.sun.corba.se.impl.orbutil.RepositoryIdUtility;
 
 public class JdbcTest {
 
@@ -14,7 +21,7 @@ public class JdbcTest {
 			Class.forName("oracle.jdbc.driver.OracleDriver");
 			Connection conn = DriverManager.getConnection(
 					"jdbc:oracle:thin:@//localhost:1521/orcl", "scott", "scottNew");
-			System.out.println(conn != null);
+//			System.out.println(conn != null);
 			
 			Statement stat = conn.createStatement();
 			String empno = "25 or 1=1";
@@ -25,14 +32,55 @@ public class JdbcTest {
 //				System.out.println(no + " " + name);
 //			}
 			
-			PreparedStatement preparedStatemenet = conn.prepareStatement("select * from emp where empno = ?");
-			preparedStatemenet.setInt(1, 12);
+			//select * from content where text = 'blahblah' or 1 = 1
+			PreparedStatement preparedStatemenet = conn.prepareStatement("select * from emp"); 
+//					conn.prepareStatement("select * from emp where empno = ?");
+//			preparedStatemenet.setInt(1, 12);
 			ResultSet rs = preparedStatemenet.executeQuery();
+			ResultSetMetaData rsmd = rs.getMetaData();
 			while(rs.next()){
-				String name = rs.getString("ename");
-				Integer no = rs.getInt("empno");
-				System.out.println(no + " " + name);
+				for (int i = 1; i <= rsmd.getColumnCount(); i++){
+//					Object obj = rs.getObject(i);
+					Object obj = null;
+					switch(rsmd.getColumnType(i)){
+						case Types.VARCHAR : obj = rs.getString(i); break;
+						case Types.NUMERIC : obj = rs.getBigDecimal(i); break;
+						case Types.DATE : obj = rs.getDate(i); break;
+						default : obj = rs.getObject(i);
+					}
+					System.out.print(obj + ";");
+				}
+//				String name = rs.getString("ename");
+//				Integer no = rs.getInt("empno");
+				System.out.println();
 			}
+			DatabaseMetaData dbmd = conn.getMetaData();
+			conn.setAutoCommit(false);
+			try {
+				// stat1.execute()
+				Savepoint savepoint1 = conn.setSavepoint("transaction1");
+				// stat2.executeQuery()
+				Savepoint savepoint2 = conn.setSavepoint("transaction2");
+				// stat3.execute()
+				Savepoint savepoint3 = conn.setSavepoint("transaction3");
+				if (true){
+					conn.rollback(savepoint2);
+				}
+				else {
+					conn.rollback(savepoint3);
+				}
+				conn.commit();
+			}
+			catch(Exception e){
+				conn.rollback();
+			}
+			
+//			CallableStatement callableStatement = conn.prepareCall("{call raise_application_error(?, ?)}");
+//			callableStatement.setInt(1, -20010);
+//			callableStatement.setString(2, "Вылетела птичка!!");
+//			callableStatement.execute();
+//			
+			
 			
 //			stat.execute("insert into emp(empno, ename) values (25, 'IvanovIvan')");
 			
